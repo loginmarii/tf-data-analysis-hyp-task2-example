00f13828376ab5c4b92569ee8b687d90df3fb4f9
loginmarii <129678289+loginmarii@users.noreply.github.com>
2023-04-13 21:04:04 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,13 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1676035524 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array, y: np.array) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    from scipy.stats import ks_2samp
+    stat, p = ks_2samp(x, y)
+    if p <= 0.08:
+        b = True
+    else:
+        b = False
+    return b # Ваш ответ, True или False
